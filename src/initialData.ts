bb2b04ff0a9fab2e76261e95cc404d01ee0180c8
x28ccddty5-rgb <x28ccddty5@privaterelay.appleid.com>
2026-06-07 19:20:41 -0300

    Delete src directory

--- src/initialData.ts
+++
@@ -1,5 +0,0 @@
-import { Activity, Stoppage, ProductionLog } from './types';
-
-export const INITIAL_ACTIVITIES: Activity[] = [];
-export const INITIAL_STOPPAGES: Stoppage[] = [];
-export const INITIAL_LOGS: ProductionLog[] = [];
