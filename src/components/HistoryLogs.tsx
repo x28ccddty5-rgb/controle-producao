b2e82eb07d518be66d1af98498eb7bf245fd2969
x28ccddty5-rgb <x28ccddty5@privaterelay.appleid.com>
2026-06-12 15:31:09 -0300

    Update HistoryLogs.tsx

--- src/components/HistoryLogs.tsx
+++ src/components/HistoryLogs.tsx
@@ -311,7 +311,7 @@ export default function HistoryLogs({
                 Nenhuma atividade encontrada com os filtros selecionados. Efetue novos lançamentos para visualizar aqui.
               </div>
             ) : (
-              <div className="overflow-x-auto">
+              <div className="overflow-x-auto overflow-y-auto max-h-[650px]">
                 <table className="w-full text-left text-xs border-collapse">
                   <thead>
                     <tr className="border-b border-slate-200 text-[10px] font-bold text-slate-500 uppercase tracking-wider select-none bg-slate-50">
@@ -389,7 +389,7 @@ export default function HistoryLogs({
                 Nenhuma parada encontrada com os filtros selecionados.
               </div>
             ) : (
-              <div className="overflow-x-auto">
+              <div className="overflow-x-auto overflow-y-auto max-h-[650px]">
                 <table className="w-full text-left text-xs border-collapse">
                   <thead>
                     <tr className="border-b border-slate-200 text-[10px] font-bold text-slate-500 uppercase tracking-wider select-none bg-slate-50">
